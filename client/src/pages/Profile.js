79a87fa9282ed0f223bc1c60ccc2ffd89b9327fe
Pao Her <p.her007@hotmail.com>
2022-04-27 23:00:16 -0500

    create a Thought Form component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,11 +4,17 @@ import { Navigate, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
+import { ADD_FRIEND } from '../utils/mutations';
+import ThoughtForm from '../components/ThoughtForm';
+
+
 
 const Profile = (props) => {
+
+const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
 
@@ -42,13 +48,28 @@ const Profile = (props) => {
   if (loading) {
     return <div>Loading...</div>;
   }
-
+  
+  const handleClick = async () => {
+      try{
+          await addFriend({
+              variables: { id: user._id}
+          });
+      } catch(e) {
+          console.error(e);
+      }
+  }
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {user.username}'s profile.
         </h2>
+        {userParam && (
+              <buton className="btn ml-auto" onClick={handleClick}>
+              Add Friend
+          </buton>
+        )}
+      
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -67,6 +88,7 @@ const Profile = (props) => {
           />
         </div>
       </div>
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
